38cded0e2971c8e4dade2ec0a84ace4c7731280c
Valentin Matev <vnmatev@gmail.com>
2024-02-21 12:01:34 +0200

    no changes

--- src/online/pok/impl/PokReaderDpfSharePrices.java
+++ src/online/pok/impl/PokReaderDpfSharePrices.java
@@ -2,7 +2,7 @@ package online.pok.impl;
 
 public class PokReaderDpfSharePrices  extends PokReaderSingleSharePrices   {
 
-    protected String getFundSurchKeyWord() {
+     protected String getFundSurchKeyWord() {
         return  PokReaderDefines.FUND_DPF_KEY;
     }
 }
